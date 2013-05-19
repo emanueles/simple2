1239b556ef56a8c379c9b7e09785257328eb6752
Emanuele Santos <emanuelesantos@gmail.com>
2013-05-19 11:55:54 -0300

    Mostrando hello world usando PyQt4

--- hello.py
+++ hello.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
-#Mostra apenas ciao
+#It uses PyQt4 to display hello world
+import sys
+from PyQt4 import QtCore, QtGui
+
+class Dialog(QtGui.QDialog):
+    def __init__(self, parent=None):
+        super(Dialog, self).__init__(parent)
+        
+        #widgets
+        self.label = QtGui.QLabel("Hello World!")
+        self.button = QtGui.QPushButton("OK")
+        
+        #signals
+        self.button.clicked.connect(self.accept)
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+        self.setMinimumSize(200,150)
+        self.setWindowTitle("Hello World")
+
 if __name__ == "__main__":
-    print("Ciao, Mondo!")
+    app = QtGui.QApplication(sys.argv)
+    dialog = Dialog()
+    sys.exit(dialog.exec_())
